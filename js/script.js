00c4040389ff793a542fb0525f257d52f131bf7a
Vinicius <marques.vinid@gmail.com>
2022-03-14 03:08:33 -0300

    v1

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$( "#flexSwitchCheckChecked" ).on( "click", function( event ) {
+    $('.btn').css('background-color', '#363636')
+    $('.btn').css('color', 'white')
+    $('body').css('background-color', 'black')
+    $('.clear').css('background-color', 'red')
+  });
